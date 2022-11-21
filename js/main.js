6ba8b1717dff2b65b9b46efed72681512215637a
MegaMix_Craft <43725099+minusQuantumNeko@users.noreply.github.com>
2022-11-21 12:18:02 +0200

    Added basic functions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,51 @@
+let Gvalue; //value containing % or number, connect with slider and input boxes
 
+async function osekaiSkillIssue(res, err) {
+        
+    console.log(`Medals Calculator Error: ${err}\nResponse Status: ${res.statusCode} | ${res.statusMessage}`); //idk why would we need that but ok, also can show user some error in case this function fires
+
+}
+
+function countDecimals(value) {
+    if(Math.floor(value) === value) return 0;
+    return value.toString().split(".")[1].length || 0; 
+}
+
+function GenerateFromPercentage(percentageValue) {
+    
+    let DecimalsNum
+    if(percentageValue.includes(".")) {
+        DecimalsNum = countDecimals(percentageValue);
+    } else {
+        DecimalsNum = 0
+    }
+    if(Number.parseInt(DecimalsNum) > 2 || Number.isNaN(+Number.parseInt(DecimalsNum))) return;
+
+    if(
+        percentageValue <= 0
+        || Math.ceil(percentageValue) > 100
+        || !Number.isFinite(+percentageValue)
+    ) return;
+
+    let answerNum = Math.ceil((medalAmount/100)*percentageValue) //Medals needed for x%, show that after calculation
+
+}
+
+function GenerateFromCount(countValue) {
+
+    let DecimalsNum
+    if(countValue.includes(".")) {
+        DecimalsNum = countDecimals(countValue);
+    } else {
+        DecimalsNum = 0
+    }
+    if(Number.parseInt(DecimalsNum) > 2 || Number.isNaN(+Number.parseInt(DecimalsNum))) return;
+
+    if(
+        countValue < 1
+        || Math.ceil(Number.parseFloat(countValue)) > Number.parseFloat(medalAmount)
+    ) return;
+
+    let answerPercent = ((countValue/medalAmount)*100).toFixed(2) //Percent you'll have after getting x medals, show that after calculation
+
+}
